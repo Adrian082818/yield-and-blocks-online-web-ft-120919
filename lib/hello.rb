e83de498eca7ba7ef42d14b80949d7e678a4520b
Adrian Mohabir <mohabiradrian@gmail.com>
2019-12-12 23:36:50 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -6,5 +6,11 @@ i += 1
 end 
 end
 
-hello_t(["Tim",])# call your method here!
+
+# call your method here!
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+    end 
+    end 
 
